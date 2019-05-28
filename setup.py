99f1220da39033ef25dd265d9cda0ca9f6dc0b62
Haophdmd <hz808@nyu.edu>
2019-05-28 20:56:57 +0800

    1.1.2

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
   name = 'restpython',         # How you named your package folder (MyLib)
   packages = ['restpython'],   # Chose the same as "name"
   package_data = {'restpython': ['Lead_Field/standard_1005_LeadField.p','Lead_Field/standard_1020_LeadField.p']},
-  version = '1.1.1',      # Start with a small number and increase it with every change you make
+  version = '1.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python Implementation of Reference Electrode Standardization Technique',   # Give a short description about your library
   author = 'Hao Zhu',                   # Type in your name
   author_email = 'hz808@nyu.edu',      # Type in your E-Mail
   url = 'https://github.com/Haophdmd/restpython',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Haophdmd/restpython/archive/1.1.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Haophdmd/restpython/archive/1.1.2.tar.gz',    # I explain this later on
   keywords = ['EEG', 'REST', 'RE-REFERENCE','PYTHON','TOOLBAOX'],   # Keywords that define your package best
   install_requires=['numpy','mne'],          # I get to this in a second
 
